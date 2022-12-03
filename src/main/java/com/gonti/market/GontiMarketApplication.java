6351b26ccdbb7af5bda9d96bb3c0491f7a3767a1
Diego <diego.s.ibarra@gmail.com>
2022-12-02 19:57:41 -0700

    created entities; compra, comprasProducto, ComprasProductoPK, Producto

---
+++ src/main/java/com/gonti/market/GontiMarketApplication.java
@@ -0,0 +1,13 @@
+package com.gonti.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GontiMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GontiMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gonti/market/GontiMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonti.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GontiMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
